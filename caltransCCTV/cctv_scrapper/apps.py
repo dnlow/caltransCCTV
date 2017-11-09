636ad0a1245aeb879dc7281457ed6aa4bb10deab
Dane Low <dnlow@live.com>
2017-11-09 09:47:40 -0800

    removed django

--- caltransCCTV/cctv_scrapper/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CctvScrapperConfig(AppConfig):
-    name = 'cctv_scrapper'
--- caltransCCTV/dashboard/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Camera
-
-# Create your views here.
-
-def dashboard(request):
-    
-    return render(request, "dashboard.html", {})
